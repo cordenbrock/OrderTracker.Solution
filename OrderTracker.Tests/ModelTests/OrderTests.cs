bc6a669930f4335fed30bd9d907131c342a7699b
cordenbrock <cordenbrock@gmail.com>
2020-12-18 13:56:33 -0500

    Purposefully pass GetAll test

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -47,16 +47,7 @@ namespace OrderTracker.Tests
       List<Order> newList = new List<Order> { newOrder1, newOrder2, newOrder3 };
 
       List<Order> result = Order.GetAll();
-      foreach (Order order in newList)
-      {
-        Console.WriteLine(order);
-      };
       
-      foreach (Order order in result)
-      {
-        Console.WriteLine(order);
-      };
-
       CollectionAssert.AreEqual(newList, result);
     }
 
